7d51736883f412325c5da6b9dd471999d38bde1b
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-13 15:51:54 -0700

    Create graphql sever schema

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Types
+import types from './resolvers/types';
+
+// Prisma connection
+import prisma from './prisma';
+
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
+    resolvers: {
+        ...types,
+    },
+    context(req) {
+        return {
+            prisma,
+            req
+        }
+    }
 })
 
 server.start(() => {
---
+++ src/resolvers/types.js
@@ -0,0 +1,3 @@
+export const User = {};
+
+export const Project = {}
