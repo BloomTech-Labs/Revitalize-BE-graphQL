4f747ffbf31b1f4b2c9194675bbdf9a7e1c57844
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-13 16:15:16 -0700

    Create users query

---
+++ src/resolvers/queries/User.js
@@ -0,0 +1,20 @@
+export const User = {
+    users(parent, args, { prisma }, info) {
+		const opArgs = {};
+
+		if (args.query) {
+			opArgs.where = {
+				OR: [
+					{
+						name_contains: args.query
+					},
+					{
+						email_contains: args.query
+					}
+				]
+			};
+		}
+
+		return prisma.query.users(opArgs, info);
+	},
+}
