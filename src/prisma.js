09780a67da95fec06f97c19b77d27c86878570d3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-15 16:54:30 -0700

    Configure Prisma to utilize env variables

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers';
 const prisma = new Prisma({
 	typeDefs: 'src/generated/prisma.graphql',
 	endpoint: process.env.PRISMA_ENDPOINT,
-	// secret: '',
+	secret: process.env.PRISMA_SECRET,
 	fragmentReplacements
 });
 
