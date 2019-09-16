2d74dd9ff95adcfc43778de60db357e34d3d95eb
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-16 10:59:37 -0700

    Install oauth dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import '@babel/polyfill/noConflict';
 import { GraphQLServer } from 'graphql-yoga';
 import { resolvers, fragmentReplacements } from './resolvers';
 import prisma from './prisma';
+import passport from 'passport';
 
-const server = new GraphQLServer({
+export const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
 	resolvers,
 	context(request) {
@@ -14,6 +15,8 @@ const server = new GraphQLServer({
 	},
 	fragmentReplacements
 });
+
+server.express.use(passport.initialize());
 
 server.start({ port: process.env.PORT || 4000 }, () => {
 	console.log('GraphQL server is now running!');
