478eb91db3e2bf76e8e063230e93ba0aaf9ebb25
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-17 22:59:23 -0700

    Create updateUserProfile mutation

--- src/resolvers/mutations/UserProfile.js
+++ src/resolvers/mutations/UserProfile.js
@@ -21,5 +21,24 @@ export const UserProfile = {
 			},
 			info
 		);
-	},
+    },
+    async deleteUserProfile(parent, args, { prisma, request }, info) {
+        const profileId = getProfileId(request);
+        const userId = getUserId(request);
+
+		const projectExists = await prisma.exists.UserProfile({ id: profileId, userAccountId: userId });
+
+		if (!projectExists) {
+			throw new Error('Sorry, but there was an error while trying to delete that project');
+		}
+
+		return prisma.mutation.deleteUserProfile(
+			{
+				where: {
+					id: profileId
+				}
+			},
+			info
+		);
+	}
 };
