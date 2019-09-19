eec95fff9f1712c77156b80202123f6d38312bd1
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-19 12:33:46 -0700

    Create projects query

--- src/resolvers/queries/Project.js
+++ src/resolvers/queries/Project.js
@@ -1,4 +1,5 @@
 import { getProfileId } from '../../utils/getProfileId';
+import { async } from 'rxjs/internal/scheduler/async';
 
 export const Project = {
 	async project(parent, args, { prisma }, info) {
@@ -7,5 +8,26 @@ export const Project = {
 				id: args.id
 			}
 		}, info)
+	},
+	async projects(parent, args, { prisma }, info) {
+		const opArgs = {};
+
+		if (args.query) {
+			opArgs.where = {
+				OR: [
+					{
+						name_contains: args.query
+					},
+					{
+						city_contains: args.query
+					},
+					{
+						state_contains: args.query
+					}
+				]
+			};
+		}
+
+		return prisma.query.projects(opArgs, info)
 	}
 };
