c70bc7039749883d0442643c683032ed03146911
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-20 11:53:19 -0700

    Create facebook passport strategy

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,21 +1,21 @@
 import { prisma } from '../prisma';
 import passport from 'passport';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser((user, done) => {
-    console.log(user)
+	console.log(user);
 	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
 	const user = prisma.query.externalAccount({
-        where: {
-            accountId: id
-        }
-    })
+		where: {
+			accountId: id
+		}
+	});
 
-    done(null, user);
+	done(null, user);
 });
 
 passport.use(
@@ -27,38 +27,83 @@ passport.use(
 			proxy: true
 		},
 		async (accessToken, refreshToken, profile, done) => {
-            try {
-                const existingUser = await prisma.query.userProfiles({
-                    where: {
-                        userAccountId: profile.id
-                    }
-                })
+			try {
+				const existingUser = await prisma.query.userProfiles({
+					where: {
+						userAccountId: profile.id
+					}
+				});
 
-                if (existingUser.length) {
-                    // we already have a row with the given googleId
-                    return done(null, existingUser[0]);
-                }
+				if (existingUser.length) {
+					// we already have a row with the given googleId
+					return done(null, existingUser[0]);
+				}
 
-                // Create new account if user does not exists
-                await prisma.mutation.createExternalAccount({
-                    data: {
-                        accountId: profile.id
-                    }
-                })
+				// Create new account if user does not exists
+				await prisma.mutation.createExternalAccount({
+					data: {
+						accountId: profile.id
+					}
+				});
 
-                const userProfile = await prisma.mutation.createUserProfile({
-                    data: {
-                        email: profile.emails[0].value,
-                        userAccountId: profile.id,
-                        firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
-                        lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
-                    }
-                })
+				const userProfile = await prisma.mutation.createUserProfile({
+					data: {
+						email: profile.emails[0].value,
+						userAccountId: profile.id,
+						firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
+						lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
+					}
+				});
 
-                done(null, userProfile);
-            } catch (err) {
-                throw new Error(err);
-            }
+				done(null, userProfile);
+			} catch (err) {
+				throw new Error(err);
+			}
+		}
+	)
+);
+
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: process.env.FACEBOOK_APP_ID,
+			clientSecret: process.env.FACEBOOK_APP_SECRET,
+			callbackURL: '/auth/facebook/callback',
+			proxy: true
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				const existingUser = await prisma.query.userProfiles({
+					where: {
+						userAccountId: profile.id
+					}
+				});
+
+				if (existingUser.length) {
+					// we already have a row with the given googleId
+					return done(null, existingUser[0]);
+				}
+
+				// Create new account if user does not exists
+				await prisma.mutation.createExternalAccount({
+					data: {
+						accountId: profile.id
+					}
+				});
+
+				const userProfile = await prisma.mutation.createUserProfile({
+					data: {
+						email: profile.emails[0].value,
+						userAccountId: profile.id,
+						firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
+						lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
+					}
+				});
+
+				done(null, userProfile);
+			} catch (err) {
+				throw new Error(err);
+			}
 		}
 	)
 );
