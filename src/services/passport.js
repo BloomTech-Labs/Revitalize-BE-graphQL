19538bfbfd00e7d5a01d00073c75ed6f171fdb95
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-26 16:15:50 -0700

    Refactor passport.js file
    Refactored passportjs file by seperating concerns for each type of auth

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,51 +1,9 @@
 import passport from 'passport';
-import {oAuthGoogle} from './oauth/google';
-const FacebookStrategy = require('passport-facebook').Strategy;
 
-// OAuth
+import { oAuthGoogle } from './oauth/google';
+import { oAuthFacebook } from './oauth/facebook';
+import { oAuthTwitter } from './oauth/twitter';
+
 passport.use(oAuthGoogle);
-
-passport.use(
-	new FacebookStrategy(
-		{
-			clientID: process.env.FACEBOOK_APP_ID,
-			clientSecret: process.env.FACEBOOK_APP_SECRET,
-			callbackURL: '/auth/facebook/callback',
-			proxy: true
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			try {
-				const existingUser = await prisma.query.userProfiles({
-					where: {
-						userAccountId: profile.id
-					}
-				});
-
-				if (existingUser.length) {
-					// we already have a row with the given googleId
-					return done(null, existingUser);
-				}
-
-				// Create new account if user does not exists
-				await prisma.mutation.createExternalAccount({
-					data: {
-						accountId: profile.id
-					}
-				});
-
-				const userProfile = await prisma.mutation.createUserProfile({
-					data: {
-						email: profile.emails[0].value,
-						userAccountId: profile.id,
-						firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
-						lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
-					}
-				});
-
-				done(null, userProfile);
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	)
-);
+passport.use(oAuthFacebook);
+passport.use(oAuthTwitter);
