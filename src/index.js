fc4c262822067fdbbdb70a1acd2fae4acb55b7b1
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-26 16:15:01 -0700

    Update token routes
    Updated oauth token route for react clients

--- src/index.js
+++ src/index.js
@@ -21,6 +21,21 @@ export const server = new GraphQLServer({
 server.express.use(passport.initialize());
 
 server.express.get(
+	'/auth/facebook',
+	passport.authenticate('facebook', { scope: ['email'], authType: 'rerequest', session: false})
+);
+
+server.express.get(
+	'/auth/facebook/callback',
+	passport.authenticate('facebook', { session: false, failureRedirect: '/login' }),
+	(req, res) => {
+		console.log(req.user)
+		const token = generateToken(req.user.userAccountId, req.user.id)
+		return res.redirect(`${process.env.OAUTH_ROUTE}/oauth/${token}`);
+	}
+);
+
+server.express.get(
 	'/auth/google',
 	passport.authenticate('google', {
 		session: false,
@@ -34,7 +49,7 @@ server.express.get(
 	(req, res) => {
 		console.log(req.user)
 		const token = generateToken(req.user.userAccountId, req.user.id)
-		return res.redirect(`http://localhost:3000/oauth/${token}`);
+		return res.redirect(`${process.env.REACT_OAUTH_ROUTE}/oauth/${token}`);
 	}
 );
 
