bd5e09367bedc8a12f876f2d230e4071c6050921
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-27 16:21:02 -0700

    Configure seeds

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,10 +1,41 @@
 const { prisma } = require('../src/generated/prisma-client');
 
 async function main() {
-    await prisma.createUser({
-        email: "frank@gmail.com",
-        password: "password"
-    })
+	try {
+        const user = await prisma.createUserAccount({
+            email: 'frank@gmail.com',
+            password: 'password',
+        });
+        console.log(user)
+
+        await prisma.createUserProfile({
+            userAccountId: user.id,
+            email: user.email,
+            firstName: "Frank",
+            lastName: "Martinez",
+            profileImage: "https://i.gyazo.com/54e7ee14f83cceb6159134196775ecd1.png",
+            city: "Kelseyville",
+            zip: "95451",
+            address: "This is an address",
+            state: "Califorina",
+            projects: {
+                create: [
+                    {
+                        name: 'Murals of Hope',
+                        description: 'Atlanta gets a mural upgrade with apprentice artits showcasing their skills and process',
+                        state: 'Georgia',
+                        address: '2300 Windy Ridge Pkwy SE',
+                        zip: 30339,
+                        city: 'Atlanta',
+                        goalAmount: 250000,
+                        amountFunded: 24313,
+                    },
+                ],
+            },
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 main();
