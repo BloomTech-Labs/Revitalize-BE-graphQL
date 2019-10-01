06496b9d29491201de406a6e32e19fb5a0e74847
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-01 16:15:56 -0700

    Create upload image utility

---
+++ src/utils/uploadImage.js
@@ -0,0 +1,28 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+export const uploadImage = async picture => {
+	const { createReadStream, filename, mimetype, encoding } = await picture;
+
+	try {
+		const result = await new Promise((resolve, reject) => {
+			createReadStream().pipe(
+				cloudinary.uploader.upload_stream((error, result) => {
+					if (error) {
+						reject(error);
+					}
+
+					resolve(result);
+				}),
+			);
+		});
+		return result;
+	} catch (error) {
+		throw new Error('Sorry, but there was an issue uploading that image');
+	}
+};
