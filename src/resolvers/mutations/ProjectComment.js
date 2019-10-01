c4dd299015123a80113f979455d445f5d8704100
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-09-30 22:12:40 -0700

    Create updateProjectComment mutation

--- src/resolvers/mutations/ProjectComment.js
+++ src/resolvers/mutations/ProjectComment.js
@@ -4,18 +4,45 @@ export const ProjectComment = {
 	async createProjectComment(parent, args, { prisma, request }, info) {
 		const profileId = getProfileId(request);
 
-		return prisma.createProjectComment({
-			profile: {
-				connect: {
-					id: profileId,
+		return prisma.createProjectComment(
+			{
+				profile: {
+					connect: {
+						id: profileId,
+					},
 				},
+				project: {
+					connect: {
+						id: args.data.id,
+					},
+				},
+				comment: args.data.comment,
 			},
-			project: {
-				connect: {
+			info,
+		);
+	},
+	async updateProjectComment(parent, args, { prisma, request }, info) {
+		const profileId = getProfileId(request);
+
+		const userComment = await prisma.$exists.projectComment({
+			id: args.id,
+			profile: profileId,
+		});
+
+		if (!userComment) {
+			throw new Error('Sorry, but there was an error trying to edit that comment');
+		}
+
+		return prisma.updateProjectComment(
+			{
+				data: {
+					comment: args.data.comment,
+				},
+				where: {
 					id: args.data.id,
 				},
 			},
-			comment: args.data.comment,
-		});
+			info,
+		);
 	},
 };
