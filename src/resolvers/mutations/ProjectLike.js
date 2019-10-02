f876155a109a836934dbaf6c9b1901a4717e8201
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-02 14:08:26 -0700

    Create delete like mutation
    Create deleteProjectLike mutation which removes a like from a project

--- src/resolvers/mutations/ProjectLike.js
+++ src/resolvers/mutations/ProjectLike.js
@@ -19,4 +19,18 @@ export const ProjectLike = {
 			},
 		});
 	},
+	async deleteProjectLike(parent, args, { prisma, request }, info) {
+		const profileId = getProfileId(request);
+
+		const userProjectLikeExists = await prisma.$exists.projectLike({ id: args.id, profile: { id: profileId } });
+
+		if (!userProjectLikeExists)
+			throw new Error('Sorry, but something went wrong while trying to delete that comment');
+
+		return prisma.deleteProjectLike({
+			where: {
+				id: args.data.id,
+			},
+		});
+	},
 };
