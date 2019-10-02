40c740e251bbe0f74b15e534a7d4939865e57301
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-02 15:06:59 -0700

    Create comment like mutation
    created createProjectCommentLike, allowing users to like a comment on a projet

---
+++ src/resolvers/mutations/ProjectCommentLike.js
@@ -0,0 +1,26 @@
+import { getProfileId } from '../../utils/getProfileId';
+
+export const ProjectCommentLike = {
+	async createProjectCommentLike(parent, args, { prisma, request }, info) {
+		const profileId = getProfileId(request);
+
+		const commentExists = await prisma.$exists.projectCommentLike({ id: args.id });
+
+		if (!commentExists) throw new Error("Sorry, but that comment doesn't exist");
+
+		return prisma.createProjectCommentLike({
+			data: {
+				profile: {
+					connect: {
+						id: profileId,
+					},
+				},
+				comment: {
+					connect: {
+						id: args.id,
+					},
+				},
+			},
+		});
+	},
+};
