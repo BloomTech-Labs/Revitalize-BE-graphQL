dbba8611d97939ab9026a61f70d888f4da556aa3
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-08 20:44:14 -0700

    Bring in newly create mutation files

--- src/resolvers/mutations/index.js
+++ src/resolvers/mutations/index.js
@@ -5,6 +5,10 @@ import { Project } from './Project';
 import { ProjectComment } from './ProjectComment';
 import { ProjectDonation } from './ProjectDonation';
 import { ProjectApplicant } from './ProjectApplicant';
+import { ProjectTrade } from './ProjectTrade';
+import { ProjectStudent } from './ProjectStudent';
+import { ProjectMasterTradesman } from './ProjectMasterTradesman';
+import { ProjectTask } from './ProjectTask';
 
 export const Mutation = {
 	...UserAccount,
@@ -13,5 +17,9 @@ export const Mutation = {
 	...Project,
 	...ProjectComment,
 	...ProjectDonation,
-	...ProjectApplicant
+	...ProjectApplicant,
+	...ProjectTrade,
+	...ProjectStudent,
+	...ProjectMasterTradesman,
+	...ProjectTask
 };
