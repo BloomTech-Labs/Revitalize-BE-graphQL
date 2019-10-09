f21c1a0fbcfce7a1b90393b9a9c3da369fcee854
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-08 20:39:36 -0700

    createProjectApplicant mutation
    Create createProjectApplicant mutation, allowing users to apply to projects

--- src/resolvers/mutations/ProjectApplicant.js
+++ src/resolvers/mutations/ProjectApplicant.js
@@ -4,6 +4,10 @@ export const ProjectApplicant = {
     async  createProjectApplicant(parent, args, { prisma, request }, info) {
         const profileId = getProfileId(request);
 
+        const project = await prisma.$exists.project({ project: args.id })
+
+        if (!project) throw new Error("Sorry, but that project does not exist")
+
         return prisma.createProjectApplicant({
             data: {
                 project: {
