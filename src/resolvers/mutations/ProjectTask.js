061edc32a95db5fff3a2e2c45f25a419ef406d73
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-10 15:34:02 -0700

    Add createProjectApprenticeTask mutation
    createProjectApprenticeTask mutation allows you to add students to task that was already created

--- src/resolvers/mutations/ProjectTask.js
+++ src/resolvers/mutations/ProjectTask.js
@@ -9,7 +9,7 @@ export const ProjectTask = {
         if (!project) throw new Error("Sorry, but that project does not exist")
 
 
-        let apprentices = [];
+        let apprentices = null;
 
         if (args.data.apprentices) {
             apprentices = args.data.apprentices.map(apprentice => {
@@ -37,6 +37,26 @@ export const ProjectTask = {
             dueDate: args.data.dueDate,
             budgetHours: args.data.budgetHours,
             apprentices
+        })
+    },
+    // Add a student to a created task
+    async CreateProjectApprenticeTask(parent, args, { prisma, request }, info) {
+        const profileId = getProfileId(request);
+        const project = await prisma.$exists.project({ id: args.data.project, profile: { id: profileId } })
+        if (!project) throw new Error("Sorry, but that project does not exist")
+
+
+        return prisma.CreateProjectApprenticeTask({
+            projectTask: {
+                connect: {
+                    id: args.data.projectTask
+                }
+            },
+            profile: {
+                connect: {
+                    id: args.data.profile
+                }
+            }
         })
     }
 }
