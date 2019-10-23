c4e252caa54f88bc82fb6a12fb56f6cf21718bb6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-10-22 22:35:12 -0700

    Update createProjectTask validation

--- src/resolvers/mutations/ProjectTask.js
+++ src/resolvers/mutations/ProjectTask.js
@@ -4,9 +4,14 @@ export const ProjectTask = {
 	async createProjectTask(parent, args, { prisma, request }, info) {
 		const profileId = getProfileId(request);
 
-		const project = await prisma.$exists.project({ id: args.data.project, profile: { id: profileId } });
+		const isProjectOwner = await prisma.$exists.project({ id: args.project, profile: { id: profileId } });
+		const isProjectTradesman = await prisma.$exists.projectMasterTradesman({
+			project: { id: args.project },
+			profile: { id: profileId },
+		});
 
-		if (!project) throw new Error('Sorry, but that project does not exist');
+		if (!isProjectOwner && !isProjectTradesman && !isProjectStudent)
+			throw new Error('Sorry, but that project does not exist');
 
 		return prisma.createProjectTask({
 			project: {
